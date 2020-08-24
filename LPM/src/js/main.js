1da449b62b5d9f32354a9007d5059f5d4de36503
Randall Wyatt <randyw567@gmail.com>
2020-08-23 21:08:43 -0400

    Added new files and have todos placed where I know I need fixing.

--- LPM/src/js/main.js
+++ LPM/src/js/main.js
@@ -1,28 +1,10 @@
-$(function() {
+//TODO--fix the redirection
+function changePage(){
+    document.getElementById("sign_in").onclick = function (){
+        window.location.href= "start.html";
+    }
+}
 
-    // hide the story from view
-    $("#story").hide();
-
-    // ---- event handler ---- //
-    $("#btn-click").click(function(e) {
-
-        // grab the values from the input boxes, then append them to the DOM
-        $(".person").empty().append($("input.person").val());
-        $(".adjective").empty().append($("input.adjective").val());
-        $(".noun").empty().append($("input.noun").val());
-        $(".insect").empty().append($("input.insect").val());
-        $(".noun2").empty().append($("input.plural-noun").val());
-        $(".verb").empty().append($("input.verb").val());
-
-        // show the story
-        $("#story").show();
-
-        // empty the form's values
-        $(':input').val('');
-
-        // hide the questions
-        $("#questions").hide();
-
-    });
-
-});
+function logInAndRedirect(){
+    document.getElementById("sign_in").addEventListener("click",changePage)
+}
