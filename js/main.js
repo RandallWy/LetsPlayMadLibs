cae348207097e449c6af53aec3b17c9b687f4960
Randall Wyatt <randyw567@gmail.com>
2020-08-30 14:24:36 -0400

    Final Fixes

--- js/main.js
+++ js/main.js
@@ -24,3 +24,31 @@ function userCreate(){
         });
     });
 }
+
+function userLogin(key){
+    let itemsArray = []
+
+    localStorage.getItem('user', JSON.stringify(itemsArray))
+    let input;
+    input = JSON.parse(localStorage.setItem('user', document.getElementById("reg_username").innerText));
+    itemsArray.push(input.value)
+    localStorage.setItem('user', JSON.stringify(itemsArray))
+    if (Modernizr.localstorage) {
+
+        let txtUsername = document.getElementById('reg_username');  // Get form elements
+
+        txtUsername.value = localStorage.getItem('user'); // Elements populated
+
+        txtUsername.addEventListener('input', function () {     // Save data on keyup
+            localStorage.setItem('user', txtUsername.value);
+        }, false);
+
+    }
+    document.ready(function () {
+        $("#login-button").click(function (event) {
+            event.preventDefault();
+            window.location = "index.html";
+        });
+    });
+}
+
